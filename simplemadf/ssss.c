65ce703576e1b3ac1c8c0b8763bb3270e1022c02
vedeeka <vedeekaparab9999@gmail.com>
2025-06-21 19:36:45 +0530

    feat: implement Dijkstra's algorithm for shortest path calculation

---
+++ simplemadf/ssss.c
@@ -0,0 +1,92 @@
+
+
+#include <stdio.h>
+#include <stdbool.h>
+#define MAX 100  
+#define INF 99999
+
+int dist[MAX];
+bool s[MAX];
+
+int minDist (int n) {
+	int min = INF, index, i;
+	for (i = 1; i <= n; i++) {
+		if (!s[i] && dist[i] < min) {
+			min = dist[i];
+			index = i;
+		}
+	}
+	return index;
+}
+
+void shortestPath (int n, int cost[MAX][MAX], int v) {
+	int i, j, w;
+	for (i = 1; i <= n; i++) {
+		s[i] = false;
+		dist[i] = cost[v][i];
+	}
+	s[v] = true;
+	dist[v] = 0;
+
+	
+	
+	for (i = 2; i <= n; i++) {
+		int u = minDist(n);
+		s[u] = true;
+		
+
+		
+		for (w = 1; w <= n; w++) {
+			if (!s[w] && cost[u][w] && dist[u] != INF && dist[w] > dist[u] + cost[u][w]) {
+				dist[w] = dist[u] + cost[u][w];
+			}
+		}
+		
+
+	}
+}
+
+int main () {
+	int n, maxE, i, j, o, d, w, start	;
+	int cost[MAX][MAX];
+	
+	printf("\nEnter the number of vertices: ");
+    scanf("%d", &n);
+    maxE = n * (n - 1);
+
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
+            cost[i][j] = INF;
+        }
+    }
+
+    for (i = 1; i <= maxE; i++) {
+        printf("Enter edge %d (-1, -1 to quit): ", i);
+        scanf("%d%d", &o, &d);
+        if (o == -1 && d == -1) {
+            break;
+        }
+        if (o > n || d > n || o < 0 || d < 0) {
+            printf("\nInvalid Input");
+            i--;
+        } else {
+            printf("Enter weight of edge: ");
+            scanf("%d", &w);
+            cost[o][d] = w;
+        }
+    }
+    
+    printf ("\nEnter the starting vertex: ");
+    scanf ("%d", &start);
+    
+    shortestPath (n, cost, start);
+    
+    printf("\nVertex\tShortest Distance from %d\n", start);
+    for (i = 1; i <= n; i++) {
+        printf("%d\t", i);
+        if (dist[i] == INF)
+            printf("INF\n");
+        else
+            printf("%d\n", dist[i]);
+    }
+}
