fd79beff0307b20b2ebdf8ccf2bd1e968dc27f80
vedeeka <vedeekaparab9999@gmail.com>
2025-06-26 12:19:58 +0530

    Refactor Huffman coding implementation and update VSCode settings

--- pract.c
+++ pract.c
@@ -1,129 +1,67 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#define MAX 256
+#include<stdio.h>
 
-typedef struct Node {
-char ch;
-int freq;
-int l;
-struct Node *left, *right;
-int order;
-} Node;
+int L[12][12];
 
-typedef struct {
-    Node* data[MAX];
-    int size;
-} PQ;
-
-int freq[MAX] = {0}, order_count = 0;
-
-Node* createNode(char ch, int freq, int isLeaf) {
-    Node* node = (Node*)malloc(sizeof(Node));
-    node->ch = ch;
-    node->freq = freq;
-    node->left = node->right = NULL;
-    node->l = isLeaf;
-    node->order = order_count++;
-    return node;
-}
-
-
-
-int compare(Node* a, Node* b) {
-    if (a->freq != b->freq) return a->freq - b->freq;
-    if (a->l && b->l) return a->ch - b->ch;
-    if (a->l != b->l) return a->l ? -1 : 1;
-    return a->order - b->order;
-}
-
-void insertQueue(PQ* q, Node* node) {
-    int i = q->size++;
-    while (i > 0 && compare(node, q->data[(i - 1) / 2]) < 0) {
-        q->data[i] = q->data[(i - 1) / 2];
-        i = (i - 1) / 2;
+int lsc(char x[], char y[]) {
+    for (int i = 0; i <= 12; i++) {
+        L[i][0] = 0;
     }
-    q->data[i] = node;
-}
-
-Node* removeMin(PQ* q) {
-Node *min =q->data[0];
-Node *last =q->data[--q->size];
-int i=0;
-int child;
-while(i*2-1<q->size){
-    child   =i*2+1;
-    if(child+1<q->size  && compare(q->data[child+1], q->data[child])<0)
-        child++;
-        if(compare(last,q->data[child])<=0) break;
-        q->data[i]=q->data[child];
-   
-        i=child;
-}
-q->data[i]=last;
-return min;
-}
-
-void generateCodes(Node* root, char* code, int depth, char codes[MAX][MAX]) {
-    if (!root) return;
-    if (root->l) {
-        code[depth] = '\0';
-        strcpy(codes[(unsigned char)root->ch], code);
-        return;
+    for (int i = 0; i <= 10; i++) {
+        L[0][i] = 0;
     }
-    code[depth] = '0';
-    generateCodes(root->left, code, depth + 1, codes);
-    code[depth] = '1';
-    generateCodes(root->right, code, depth + 1, codes);
-}
 
-Node* buildHuffmanTree(char* str) {
-PQ q;
-    q.size = 0;
-
-for(int i=0;i<MAX;i++){
-    freq[(unsigned char)str[i]]++;
-}
-
-for(int i = 0; i < MAX; i++) {
-    if (freq[i] > 0) {
-        insertQueue(&q, createNode((char)i, freq[i], 1));
-    }
-}
-
-if(q.size >0) {
-Node *left, *right, *newNode;
-    while (q.size > 1) {
-        left = removeMin(&q);
-        right = removeMin(&q);
-        newNode = createNode('\0', left->freq + right->freq, 0);
-        newNode->left = left;
-        newNode->right = right;
-        insertQueue(&q, newNode);
-    }
-    return q.data[0];
-}
-    return removeMin(&q);
-}
-
-int main() {
-    char str[1000], codes[MAX][MAX] = {{0}}, buffer[MAX];
-
-    printf("Enter the input string: ");
-    scanf(" %[^\n]", str);
-
-    Node* root = buildHuffmanTree(str);
-    generateCodes(root, buffer, 0, codes);
-
-    printf("\nHuffman Codes:\nCharacter | Code\n------------------------\n");
-    for (int i = 0; i < MAX; i++) {
-        if (freq[i] > 0) {
-            if (i == ' ')
-                printf("   sp     | %s\n", codes[i]);
-            else
-                printf("    %c     | %s\n", i, codes[i]);
+    for (int i = 1; i < 12; i++) {
+        for (int j = 1; j < 10; j++) {
+            if (x[j] == y[i]) {
+                L[i][j] = L[i - 1][j - 1] + 1;
+            } else {
+                int max;
+                if (L[i - 1][j] > L[i][j - 1]) {
+                    max = L[i - 1][j];
+                } else {
+                    max = L[i][j - 1];
+                }
+                L[i][j] = max;
+            }
         }
     }
 
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 10; j++) {
+            printf("%d ", L[i][j]);
+        }
+        printf(" \n");
+    }
+
+    // Backtrack to find LCS
+    int i = 11, j = 9;
+    char lcs[12]; // Max LCS size is min(10,12)
+    int index = 0;
+
+    while (i > 0 && j > 0) {
+        if (y[i] == x[j]) {
+            lcs[index++] = x[j];
+            i--;
+            j--;
+        } else if (L[i - 1][j] > L[i][j - 1]) {
+            i--;
+        } else {
+            j--;
+        }
+    }
+
+    printf("LCS: ");
+    for (int k = index - 1; k >= 0; k--) {
+        printf("%c", lcs[k]);
+    }
+    printf("\n");
+
     return 0;
+}
+
+int main() {
+    char t1[10] = { 'g','t','t','c','c','t','a','a','t','a' };
+    char t2[12] = { 'c','g','a','t','a','a','t','t','g','a','g','a' };
+
+    lsc(t1, t2);
 }
--- simplemadf/pattern matching/lcs.c
+++ simplemadf/pattern matching/lcs.c
@@ -1,47 +1,64 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 
-int L[12][12];
+int L[100][100];
 
+void printLCS(char t1[], char t2[], int m, int n) {
+    int i = m, j = n;
+    char lcs[100];
+    int index = L[m][n];
+    lcs[index] = '\0';  // null-terminate the string
+    index--;
 
-int lsc(char x[],char y[]){
-
-    for (int i=0;i<=12;i++){
-        L[i][0]=0;
+    while (i > 0 && j > 0) {
+        if (t1[i - 1] == t2[j - 1]) {
+            lcs[index--] = t1[i - 1];
+            i--; j--;
+        } else if (L[i - 1][j] > L[i][j - 1]) {
+            i--;
+        } else {
+            j--;
+        }
     }
-    for (int i=0;i<=10;i++){
-        L[0][i]=0;
-    }
 
-    for(int i=0;i<12;i++){
-        for(int j=0;j<10;j++){
-            if(x[i]==y[j]){
-                L[i][j]=L[i-1][j-1]+1;
+    printf("\nLongest Common Subsequence: %s\n", lcs);
+}
 
-            }else{
-                int max;
-                if(L[i-1][j]>L[i][j-1]){
-                    max=L[i-1][j];
-                }else{
-                     max=L[i][j-1];
-                }
-                L[i][j]=max;
+int lsc(char t1[], char t2[], int m, int n) {
+    for (int i = 0; i <= m; i++) L[i][0] = 0;
+    for (int j = 0; j <= n; j++) L[0][j] = 0;
+
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (t1[i - 1] == t2[j - 1]) {
+                L[i][j] = L[i - 1][j - 1] + 1;
+            } else {
+                L[i][j] = (L[i - 1][j] > L[i][j - 1]) ? L[i - 1][j] : L[i][j - 1];
             }
         }
     }
 
-    for(int i=0;i<12;i++){
-        for(int j=0;j<10;j++){
-            printf("%d ",L[i][j]);
+    // Optional: Print LCS matrix
+    printf("LCS Matrix:\n");
+    for (int i = 0; i <= m; i++) {
+        for (int j = 0; j <= n; j++) {
+            printf("%d ", L[i][j]);
         }
-        printf(" \n");
+        printf("\n");
     }
+
+    printLCS(t1, t2, m, n);
     return 0;
 }
 
+int main() {
+    // Add null-terminated versions of the sequences
+    char t1[] = "gttcctaata";         // length = 10
+    char t2[] = "cgataattgaga";       // length = 12
 
-int main(){
-    char t1[10]={'g','t','t','c','c','t','a','a','t','a'};
-    char t2[12]={'c','g','a','t','a','a','t','t','g','a','g','a'};
+    int m = strlen(t1);
+    int n = strlen(t2);
 
-    lsc(t1,t2);
+    lsc(t1, t2, m, n);
+    return 0;
 }
